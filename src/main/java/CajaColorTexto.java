7180e53c892d46b1248f9a8d08c55e0d9846d94b
a21angelmb <email>
2022-11-14 18:42:58 +0100

    Casi terminado. Falta negrita-cursiva, agrupar botones, colocar texto y limpiar código.

--- src/main/java/CajaColorTexto.java
+++ src/main/java/CajaColorTexto.java
@@ -4,8 +4,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CajaColorTexto extends JFrame implements ActionListener {
-    private JLabel textoDeFondo; JRadioButtonMenuItem fondoBlanco; JRadioButtonMenuItem fondoAmarillo; JRadioButtonMenuItem fondoAzul;
-    private JPanel fondo; JCheckBoxMenuItem negrita; JCheckBoxMenuItem cursiva;
+    private JLabel texto; JRadioButtonMenuItem fondoBlanco; JRadioButtonMenuItem fondoAmarillo; JRadioButtonMenuItem fondoAzul;
+    JRadioButtonMenuItem rojo; JRadioButtonMenuItem azul; JRadioButtonMenuItem negro; JRadioButtonMenuItem verde; JRadioButtonMenuItem sansSerif;
+    JPanel fondo; JCheckBoxMenuItem negrita; JCheckBoxMenuItem cursiva; JRadioButtonMenuItem serif; JRadioButtonMenuItem monospaced;
+
 
     public CajaColorTexto(){
         JMenuBar menuBar = new JMenuBar();
@@ -18,17 +20,18 @@ public class CajaColorTexto extends JFrame implements ActionListener {
         JMenuItem salir = new JMenuItem("Salir");
         JMenu formato = new JMenu("Formato");
         JMenu color = new JMenu("Color");
-        JRadioButtonMenuItem negro = new JRadioButtonMenuItem("Negro");
-        JRadioButtonMenuItem azul = new JRadioButtonMenuItem("Azul");
-        JRadioButtonMenuItem rojo = new JRadioButtonMenuItem("Rojo");
-        JRadioButtonMenuItem verde = new JRadioButtonMenuItem("Verde");
+        negro = new JRadioButtonMenuItem("Negro");
+        azul = new JRadioButtonMenuItem("Azul");
+        rojo = new JRadioButtonMenuItem("Rojo");
+        verde = new JRadioButtonMenuItem("Verde");
         JMenu tipoDeLetra = new JMenu("Tipo de letra");
-        JRadioButtonMenuItem serif = new JRadioButtonMenuItem("Serif");
-        JRadioButtonMenuItem monospaced = new JRadioButtonMenuItem("Monospaced");
-        JRadioButtonMenuItem sansSerif = new JRadioButtonMenuItem("SansSerif");
+        serif = new JRadioButtonMenuItem("Serif");
+        monospaced = new JRadioButtonMenuItem("Monospaced");
+        sansSerif = new JRadioButtonMenuItem("SansSerif");
         negrita = new JCheckBoxMenuItem("Negrita");
         cursiva = new JCheckBoxMenuItem("Cursiva");
 
+        //Añado colores de fondo al menú, desmarcados por defecto y añado listener
 
         fondoBlanco.setSelected(false); cambiarFondo.add(fondoBlanco); fondoAmarillo.setSelected(false);
         cambiarFondo.add(fondoAmarillo); fondoAzul.setSelected(false); cambiarFondo.add(fondoAzul);
@@ -38,9 +41,13 @@ public class CajaColorTexto extends JFrame implements ActionListener {
 
         menuBar.add(archivo);
 
+        //Añado colores de fuente al menú, desmarcados por defecto y añado listener
+
         negro.setSelected(false);negro.addActionListener(this); azul.setSelected(false); azul.addActionListener(this);
         rojo.setSelected(false); rojo.addActionListener(this); verde.setSelected(false); verde.addActionListener(this);
         color.add(negro); color.add(azul); color.add(rojo);color.add(verde);
+
+        //Añado fuentes al menú, desmarcados por defecto y añado listener
 
         serif.setSelected(false); serif.addActionListener(this); monospaced.setSelected(false); monospaced.addActionListener(this); sansSerif.setSelected(false);sansSerif.addActionListener(this);
         tipoDeLetra.add(serif); tipoDeLetra.add(monospaced); tipoDeLetra.add(sansSerif);
@@ -51,13 +58,13 @@ public class CajaColorTexto extends JFrame implements ActionListener {
         menuBar.add(formato); this.setJMenuBar(menuBar);
 
 
-        textoDeFondo = new JLabel("Texto de fondo");
+        texto = new JLabel("Texto de fondo");
 
 
         fondo = new JPanel();
         fondo.setBackground(Color.cyan);
         fondo.setLayout(new FlowLayout());
-        fondo.add(textoDeFondo);
+        fondo.add(texto);
 
 
 
@@ -74,18 +81,39 @@ public class CajaColorTexto extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
 
+        //cambiar color de fondo
+
         if (e.getSource().equals(fondoBlanco)) {
             fondo.setBackground(Color.white);
         } else if (e.getSource().equals(fondoAmarillo)) {
             fondo.setBackground(Color.yellow);
-        } else {
+        } else if (e.getSource().equals((fondoAzul))) {
             fondo.setBackground(Color.cyan);
         }
 
-        if (e.getSource().equals(negrita)){
-            textoDeFondo.
+        //cambiar color de texto
+
+        if (e.getSource().equals(rojo)) {
+            texto.setForeground(Color.red);
+        } else if (e.getSource().equals(verde)) {
+            texto.setForeground(Color.green);
+        } else if (e.getSource().equals(negro)) {
+            texto.setForeground(Color.black);
+        } else if (e.getSource().equals(azul)) {
+            texto.setForeground(Color.blue);
         }
 
+        //cambiar fuente de texto
+
+        if (e.getSource().equals(serif)) {
+            texto.setFont(new Font("Serif", Font.PLAIN, 50));
+
+        } else if (e.getSource().equals(sansSerif)) {
+            texto.setFont(new Font("SansSerif", Font.PLAIN, 50));
+
+        } else if (e.getSource().equals(monospaced)) {
+            texto.setFont(new Font("Monospaced", Font.PLAIN, 50));
+        }
     }
 
 
